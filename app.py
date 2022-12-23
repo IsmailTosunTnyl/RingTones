87c7161f8b9b06421aaf7212d62db84026c0bd5f
itosun <ismailtosuntnyl@gmail.com>
2022-12-23 16:42:19 +0300

    Co-authored-by: MemreYlmz <MemreYlmz@users.noreply.github.com> Co-authored-by: Zafer Say <zafersay93@gmail.com>

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask , render_template,session
+from flask_session import Session
+
+import DB
+
+db = DB.DataBase()
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 @app.route("/")
 def hello_world():
-    return "<p>siktik</p>"
-
+    data = dict(db.find_user_with_email('mail1','6060'))
+    
+    return render_template("index.html", data=data)
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80,debug=True)
